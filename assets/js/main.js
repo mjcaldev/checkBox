17e4bd0b83aad23ef5e390eb6e627992b81e0420
Micheal Callaghan <mehaul96@gmail.com>
2025-01-23 01:17:07 -0500

    finished JS using boolean logic to ensure omnidirectional fill between checked items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
-const checkBoxes = document.queryselector('.inbox input[ type="checkbox"]');
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 function handleCheck(e) {
-  console.log(e )
+  // Check if they had the shift down
+  // Check it is being checked not unchecked
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+  // Loop over items between two checked items
+  checkBoxes.forEach(box => {
+    console.log(box)
+    if (box === this || box === lastChecked) {
+      inBetween = !inBetween;
+      console.log('Checking inbetween!')
+    }
+    if (inBetween) {
+      box.checked = true;
+    }
+  });
+}
+
+lastChecked = this;
 }
 
 checkBoxes.forEach(box => box.addEventListener('click', handleCheck)); // click fires even on keyboard
